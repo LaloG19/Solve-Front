5176e540ed50a21b19de349cff61f898e9fe0b51
Andy Isaac <122947250+Andy-Castle@users.noreply.github.com>
2024-04-16 13:58:25 -0500

    ft: Módulo Horarios (#3)
    * ft: se agrego la interfaz, el servicio, el store y un coponente de schedules, por ahora la unica revisada es getAllSchedules, el deleteSchedule no funciona aun, no creo porque me hacer falta ponerle "/delete" a las routes del back, porque entre al swagger y si elimina, seguire viendo para corregirlo
    
    * up: ya sirven las funciones necesarias de schedules module, talvez un poco de ayuda en el css no me caeria mal. PDST: Como que soy el unico gil usando TS >:V

---
+++ src/modules/schedules/interfaces/ISchedule.ts
@@ -0,0 +1,12 @@
+export interface ISchedule {
+  scheduleID?: number
+  entryTime: string
+  exitTime: string
+  monday: boolean
+  tuesday: boolean
+  wednesday: boolean
+  thursday: boolean
+  friday: boolean
+  saturday: boolean
+  sunday: boolean
+}
